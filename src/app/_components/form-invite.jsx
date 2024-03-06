64d59d76ce562fe3c3d75bc3953bd7f77c5bfa89
Jordan <jordan-souchez@hotmail.fr>
2024-03-06 18:54:47 +0100

    remove button on submit

--- src/app/_components/form-invite.jsx
+++ src/app/_components/form-invite.jsx
@@ -45,10 +45,14 @@ function FormInvite() {
             <Image src="/diddle-cake.png" height={120} width={127} className="absolute -top-1/2 right-0" />
             <label htmlFor="name" className="text-2xl font-bold mt-4">Tu viens ?</label>
             <input ref={inputRef} type="text" id="name" name="name" placeholder="Ton petit nom (ex: Jeanne)" className="px-3 py-2 text-lg my-3 rounded-xl border border-pink-400" />
-            <div className="flex gap-4 mt-2">
+            {isDone ? (
+                <div className="flex-1 bg-pink-50 py-2 rounded-xl text-center">
+                    <p>Merci d'avoir répondu !</p>
+                </div>
+            ) : (<div className="flex gap-4 mt-2">
                 <button disabled={isDone} type="button" onClick={handleAccept} className="flex-1 py-2 rounded-xl bg-pink-400">Avec plaisir</button>
                 <button disabled={isDone} type="button" onClick={handleDecline} className="flex-1 py-2 rounded-xl bg-pink-400">Non désolé</button>
-            </div>
+            </div>)}
         </form>
     )
 }
